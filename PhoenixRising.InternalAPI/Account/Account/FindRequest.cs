983702580f8fcd5584cc7c9e412b608c8e5d4fce
Knar33 <adbyerly@comcast.net>
2018-05-03 22:26:10 -0500

    Created empty classes

---
+++ PhoenixRising.InternalAPI/Account/Account/FindRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixRising.InternalAPI.Account.Account
+{
+    public class FindRequest
+    {
+        public FindRequest(AuthenticationStore auth)
+        {
+
+        }
+
+        public AuthenticationStore Auth { get; set; }
+
+        public FindResponse Send()
+        {
+            return new FindResponse();
+        }
+    }
+}

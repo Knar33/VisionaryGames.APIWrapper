06b022e34530acd620efee65856bbcdf9212e2a1
Knar <knarxr@protonmail.com>
2018-05-03 22:26:10 -0500

    Created empty classes

---
+++ PhoenixRising.InternalAPI.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoenixRising.InternalAPI;
+using PhoenixRising.InternalAPI.Account.Account;
+
+namespace PhoenixRising.InternalAPI.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Guid user = new Guid("66e0db28-3fd9-6653-56dc-f02aaa393ab3");
+            string accessToken = "accesstokenhere";
+            int expiresTime = 12345678;
+            string refreshToken = "refreshtokenhere";
+
+            AuthenticationStore auth = new AuthenticationStore(user, accessToken, expiresTime, refreshToken);
+
+            FindRequest request = new FindRequest(auth);
+            FindResponse response = request.Send();
+        }
+    }
+}
---
+++ PhoenixRising.InternalAPI/AuthenticationStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixRising.InternalAPI
+{
+    public class AuthenticationStore
+    {
+        public AuthenticationStore()
+        {
+        }
+
+        public AuthenticationStore(Guid userID, string accessToken, int expiresTime, string refreshToken)
+        {
+            UserID = userID;
+            AccessToken = accessToken;
+            ExpireTime = expiresTime;
+            RefreshToken = refreshToken;
+        }
+
+        public Guid UserID { get; set; }
+        public string AccessToken { get; set; }
+        public int ExpireTime { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}

841ed3a5d9a14b7cf09bfc72cb3cd94ccfca8233
Knar33 <adbyerly@comcast.net>
2018-05-06 21:34:55 -0500

    Built Account/Find method. Hard coded URL for now

--- PhoenixRising.InternalAPI.Tests/Tests.cs
+++ PhoenixRising.InternalAPI.Tests/Tests.cs
@@ -12,14 +12,16 @@ namespace PhoenixRising.InternalAPI.Tests
         public void TestMethod()
         {
             Guid user = new Guid("66e0db28-3fd9-6653-56dc-f02aaa393ab3");
-            string accessToken = "accesstokenhere";
+            string accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjY2ZTBkYjI4LTNmZDktNjY1My01NmRjLWYwMmFhYTM5M2FiMyIsImV4cCI6MTUyMDY5NjcxMSwiaXNzIjoiYXBpLnZpc2lvbmFyeWdhbWVzLnh5eiIsImF1ZCI6InZpc2lvbmFyeWdhbWVzLnh5eiJ9.Hb193v4bDX78YVvYH-qvYGN0JAPOeGAqWiAKf4H80a0";
             int expiresTime = 12345678;
             string refreshToken = "refreshtokenhere";
-
             AuthenticationStore auth = new AuthenticationStore(user, accessToken, expiresTime, refreshToken);
 
-            FindRequest request = new FindRequest(auth);
+            FindRequest request = new FindRequest("linternator", auth);
             FindResponse response = request.Send();
         }
+
+
+
     }
 }

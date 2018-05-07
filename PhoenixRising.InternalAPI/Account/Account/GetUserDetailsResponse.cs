819f680a90393c0fcb9815e07df6268d9fa59a6b
Knar <knarxr@protonmail.com>
2018-05-06 23:08:47 -0500

    Added smarter handling for bad requests.

--- PhoenixRising.InternalAPI/Account/Account/GetUserDetailsResponse.cs
+++ PhoenixRising.InternalAPI/Account/Account/GetUserDetailsResponse.cs
@@ -4,11 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using RestSharp;
 
 namespace PhoenixRising.InternalAPI.Account.Account
 {
     public class GetUserDetailsResponse
     {
+        public GetUserDetailsResponse(IRestResponse<GetUserDetailsResponse> res)
+        {
+            StatusCode = res.StatusCode;
+            if (res.StatusCode == HttpStatusCode.OK)
+            {
+                NICKNAME = res.Data.NICKNAME;
+                FIRST_NAME = res.Data.FIRST_NAME;
+                LAST_NAME = res.Data.LAST_NAME;
+                EMAIL = res.Data.EMAIL;
+                CURRENCY_AMOUNT = res.Data.CURRENCY_AMOUNT;
+                IS_GAME_DEV = res.Data.IS_GAME_DEV;
+                LEVEL = res.Data.LEVEL;
+                CURRENCY_AMOUNT_2 = res.Data.CURRENCY_AMOUNT_2;
+                ONLINE_STATUS = res.Data.ONLINE_STATUS;
+                GAME_STATUS = res.Data.GAME_STATUS;
+                PERMISSIONS = new Permissions
+                {
+                    Developer = res.Data.PERMISSIONS.Developer,
+                    Administrator = res.Data.PERMISSIONS.Administrator,
+                    Banned = res.Data.PERMISSIONS.Banned
+                };
+            }
+            else
+            {
+                Content = res.Content;
+            }
+        }
+
+        public GetUserDetailsResponse()
+        {
+
+        }
+
         public string Content { get; set; }
         public HttpStatusCode StatusCode {get; set;}
 
@@ -23,5 +57,13 @@ namespace PhoenixRising.InternalAPI.Account.Account
         public string CURRENCY_AMOUNT_2 { get; set; }
         public string ONLINE_STATUS { get; set; }
         public string GAME_STATUS { get; set; }
+        public Permissions PERMISSIONS { get; set; }
+    }
+
+    public class Permissions
+    {
+        public bool Developer { get; set; }
+        public bool Administrator { get; set; }
+        public bool Banned { get; set; }
     }
 }

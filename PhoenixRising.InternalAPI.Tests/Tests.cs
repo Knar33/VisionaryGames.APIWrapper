1e1ab6d4a653ff338d4fefa8b23a4eaad4196a7d
Knar <knarxr@protonmail.com>
2018-05-07 20:10:04 -0500

    Added RestSharp

--- PhoenixRising.InternalAPI.Tests/Tests.cs
+++ PhoenixRising.InternalAPI.Tests/Tests.cs
@@ -32,5 +32,20 @@ namespace PhoenixRising.InternalAPI.Tests
             GetUserDetailsRequest request = new GetUserDetailsRequest(auth, connection);
             GetUserDetailsResponse response = request.Send();
         }
+
+        [TestMethod]
+        public void Ping()
+        {
+            Guid user = new Guid("46e0db28-3fd9-6653-56dc-f02aaa393ab3");
+            string accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjY2ZTBkYjI4LTNmZDktNjY1My01NmRjLWYwMmFhYTM5M2FiMyIsImV4cCI6MTUyNTY2NjE5NSwiaXNzIjoiYXBpLnZpc2lvbmFyeWdhbWVzLnh5eiIsImF1ZCI6InZpc2lvbmFyeWdhbWVzLnh5eiJ9.lo-rrtCA052Dl476nO9UNldYzd3VcEVEegSZHWyHJyc";
+            int expiresTime = 12345678;
+            string refreshToken = "refreshtokenhere";
+            AuthenticationStore auth = new AuthenticationStore(user, accessToken, expiresTime, refreshToken);
+
+            APIConnection connection = new APIConnection("https://pr-api-uks-dev.azurewebsites.net");
+
+            GetUserDetailsRequest request = new GetUserDetailsRequest(auth, connection);
+            GetUserDetailsResponse response = request.Send();
+        }
     }
 }
--- PhoenixRising.InternalAPI/Account/Account/PingResponse.cs
+++ PhoenixRising.InternalAPI/Account/Account/PingResponse.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using RestSharp;
 
 namespace PhoenixRising.InternalAPI.Account.Account
 {
     class PingResponse
     {
+        public PingResponse(IRestResponse<PingResponse> res)
+        {
+            StatusCode = res.StatusCode;
+            if (res.StatusCode == HttpStatusCode.OK)
+            {
+                
+            }
+            else
+            {
+                Content = res.Content;
+            }
+        }
+
+        public PingResponse()
+        {
+
+        }
+
         public string Content { get; set; }
         public HttpStatusCode StatusCode { get; set; }
     }

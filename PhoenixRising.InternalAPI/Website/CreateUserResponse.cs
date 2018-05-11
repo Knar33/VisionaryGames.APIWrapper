773bf5a7ef5c6ff2a140a62b879fb5e3c233c5a7
Knar33 <adbyerly@comcast.net>
2018-05-10 22:47:25 -0500

    Added new requests

---
+++ PhoenixRising.InternalAPI/Website/CreateUserResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using RestSharp;
+
+namespace PhoenixRising.InternalAPI.Website
+{
+    class CreateUserResponse
+    {
+        public CreateUserResponse(IRestResponse<CreateUserResponse> res)
+        {
+            StatusCode = res.StatusCode;
+            if (res.StatusCode != HttpStatusCode.OK)
+            {
+                Content = res.Content;
+            }
+        }
+
+        public CreateUserResponse()
+        {
+
+        }
+
+        public string Content { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
---
+++ PhoenixRising.InternalAPI/Website/RequestResetPasswordRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using RestSharp;
+
+namespace PhoenixRising.InternalAPI.Website
+{
+    class RequestResetPasswordRequest
+    {
+        public RequestResetPasswordRequest(APIConnection connection, string nickname)
+        {
+            Connection = connection;
+            Nickname = nickname;
+        }
+
+        public string Nickname { get; set; }
+        public APIConnection Connection { get; set; }
+
+        public RequestResetPasswordResponse Send()
+        {
+            RestClient client = new RestClient(Connection.URL);
+            RestRequest request = new RestRequest("account/find/{nickname}", Method.GET);
+            request.AddUrlSegment("nickname", Nickname);
+
+            var res = client.Execute<RequestResetPasswordResponse>(request);
+
+            return new RequestResetPasswordResponse(res);
+        }
+    }
+}

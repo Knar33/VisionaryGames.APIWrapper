a02a27992b240fdbb2414577cdc9ffa2ce9ffe7f
Knar33 <adbyerly@comcast.net>
2018-05-10 22:49:53 -0500

    Added password request

--- PhoenixRising.InternalAPI/Website/RequestResetPasswordRequest.cs
+++ PhoenixRising.InternalAPI/Website/RequestResetPasswordRequest.cs
@@ -10,20 +10,20 @@ namespace PhoenixRising.InternalAPI.Website
 {
     class RequestResetPasswordRequest
     {
-        public RequestResetPasswordRequest(APIConnection connection, string nickname)
+        public RequestResetPasswordRequest(APIConnection connection, string email)
         {
             Connection = connection;
-            Nickname = nickname;
+            Email = email;
         }
 
-        public string Nickname { get; set; }
+        public string Email { get; set; }
         public APIConnection Connection { get; set; }
 
         public RequestResetPasswordResponse Send()
         {
             RestClient client = new RestClient(Connection.URL);
-            RestRequest request = new RestRequest("account/find/{nickname}", Method.GET);
-            request.AddUrlSegment("nickname", Nickname);
+            RestRequest request = new RestRequest("/app/account/passwd", Method.POST);
+            request.AddHeader("userEmail", Email);
 
             var res = client.Execute<RequestResetPasswordResponse>(request);
 

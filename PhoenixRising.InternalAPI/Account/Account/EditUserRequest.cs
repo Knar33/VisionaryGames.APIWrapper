53c57794b1ae209ec615891abb55fc98ea9e3745
Knar33 <adbyerly@comcast.net>
2018-05-18 19:32:08 -0500

    Added ResendVerification request. Added password object for change email

--- PhoenixRising.InternalAPI/Account/Account/EditUserRequest.cs
+++ PhoenixRising.InternalAPI/Account/Account/EditUserRequest.cs
@@ -24,6 +24,7 @@ namespace PhoenixRising.InternalAPI.Account.Account
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Nicknane { get; set; }
+        public string Password { get; set; }
 
         public EditUserResponse Send()
         {
@@ -33,6 +34,10 @@ namespace PhoenixRising.InternalAPI.Account.Account
             request.RequestFormat = DataFormat.Json;
             request.AddBody(new { USER_FNAME = FirstName, USER_LNAME = LastName, USER_EMAIL = Email, USER_NICK = Nicknane });
             request.AddHeader("X-Access-Token", AccessToken);
+            if (Password != null)
+            {
+                request.AddHeader("Password", Password);
+            }
 
             var res = client.Execute<EditUserResponse>(request);
 
---
+++ PhoenixRising.InternalAPI/Account/Account/ResendVerificationRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace PhoenixRising.InternalAPI.Account.Account
+{
+    public class ResendVerificationRequest
+    {
+        public ResendVerificationRequest(string connection, string accessToken, Guid userID)
+        {
+            Connection = connection;
+            AccessToken = accessToken;
+            UserID = userID;
+        }
+
+        public string Connection { get; set; }
+        public string AccessToken { get; set; }
+        public Guid UserID { get; set; }
+
+        public ResendVerificationResponse Send()
+        {
+            RestClient client = new RestClient(Connection);
+            RestRequest request = new RestRequest("account/{userID}/verify", Method.POST);
+            request.AddHeader("X-Access-Token", AccessToken);
+            request.AddUrlSegment("userID", UserID);
+
+            var res = client.Execute<ResendVerificationResponse>(request);
+
+            return new ResendVerificationResponse(res);
+        }
+    }
+}

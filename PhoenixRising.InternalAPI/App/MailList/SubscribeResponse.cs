68d6167c5217bec15b45e800b632e4560b1bbae2
Knar <knarxr@protonmail.com>
2018-05-28 17:39:14 -0500

    Subscribe/unsubscribe request/response

---
+++ PhoenixRising.InternalAPI/App/MailList/SubscribeResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using RestSharp;
+
+namespace PhoenixRising.InternalAPI.App.MailList
+{
+    public class SubscribeResponse
+    {
+        public SubscribeResponse(IRestResponse<SubscribeResponse> res)
+        {
+            StatusCode = res.StatusCode;
+            Content = res.Content;
+        }
+
+        public SubscribeResponse()
+        {
+
+        }
+
+        public string Content { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}

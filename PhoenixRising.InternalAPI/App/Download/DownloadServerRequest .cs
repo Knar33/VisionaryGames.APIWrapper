257b9d6604e589e43e15dd4fcf5b76e8957f8fce
David Lintern <david.lintern@etiltd.co.uk>
2018-05-30 12:01:48 +0100

    Added client and server download url requests.

---
+++ PhoenixRising.InternalAPI/App/Download/DownloadServerRequest .cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+
+namespace PhoenixRising.InternalAPI.App.Download
+{
+    public class DownloadServerRequest
+    {
+        public DownloadServerRequest(string connection, string appAccessToken)
+        {
+            Connection = connection;
+            AppAccessToken = appAccessToken;
+        }
+
+        public string Connection { get; set; }
+        public string AppAccessToken { get; set; }
+
+        public DownloadServerResponse Send()
+        {
+            RestClient client = new RestClient(Connection);
+            RestRequest request = new RestRequest("app/download/server", Method.GET);
+            request.AddHeader("App-Access-Token", AppAccessToken);
+
+            var res = client.Execute<DownloadClientResponse>(request);
+
+            return new DownloadServerResponse(res);
+        }
+
+    }
+}
---
+++ PhoenixRising.InternalAPI/App/Download/DownloadServerResponse.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixRising.InternalAPI.App.Download
+{
+    public class DownloadServerResponse
+    {
+
+        public DownloadServerResponse(IRestResponse<DownloadClientResponse> res)
+        {
+            StatusCode = res.StatusCode;
+            Content = res.Content;
+        }
+
+        public DownloadServerResponse()
+        {
+
+        }
+
+        public string Content { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+    }
+}
